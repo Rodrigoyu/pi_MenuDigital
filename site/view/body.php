102196c88f983255b696ee7bdc566821a131fb32
Rodrigoyu <rodrigoj1478@gmail.com>
2023-10-04 21:32:11 -0300

    corrigindo error do contado

--- site/view/body.php
+++ site/view/body.php
@@ -6,19 +6,19 @@ $itemMenu = listarItemMenu();
 <!--Fim Do Header-->
 
 <!--Inicio do cardapio-->
-<div class="container-md ">
-    <div class="container-md px-4 justify-content-center">
+<div class="container-md conteine_Pricipal ">
+    <div class="conteine_pricipal_cardapio container-md px-4 justify-content-center">
             <?php foreach ($itemMenu as $item): ?>
-                <div class="row border">
+                <div class="caixa_de_item row border">
 
-                    <div class="col justify-content-center caixa_de_item">
+                    <div class="col justify-content-center informacao_div">
                         <div class=" h2 titulo_da_caixa row ">
                             <?= $item['nome'] ?>
                         </div>
                         <div class=" col-md-5 gy-5 descricao_da_caixa">
                             <?= $item['descricao'] ?>
                         </div>
-                        <div class="preco_da_caixa row">Preço R$<div id="valor_do_preco col-md-5" class="preço_div col">
+                        <div class="preco_da_caixa row">Preço R$<div id="valor_do_preco" class="preço_div col">
                                 <?= floatval($item['valor']) ?>
                             </div>
                         </div>
@@ -32,22 +32,12 @@ $itemMenu = listarItemMenu();
 
 
 
-                    <div class="row container text-center">
-
-                        <div class="row ">
-                            <div class="col col-md-auto .quantidade_1 ">
-                                <button class="btn btn-outline-info" id="meno_button"
-                                    onclick="subtrairQuantidade(this)">-</button>
-                            </div>
-
-                            <div class="col col-md-auto border text-center .preço_div">0</div>
-
-                            <div class="col col-md-auto">
-                                <button class="btn btn-outline-info" id="mais_button"
-                                    onclick="adicionarQuantidade(this)">+</button>
-                            </div>
+                    <div class="button_div ">
+                        <div class="butao_da_caixa d-flex border-4 ">
+                            <button class="btn btn-outline-info" id="meno_button" onclick="subtrairQuantidade(this)">-</button>
+                            <div class="quantidade_1 border-4 border-left border-right">0</div>
+                            <button class="btn btn-outline-info" id="mais_button" onclick="adicionarQuantidade(this)">+</button>
                         </div>
-
                     </div>
 
 
@@ -56,12 +46,13 @@ $itemMenu = listarItemMenu();
             <?php endforeach ?>
     </div>
 
-    <div class="conteine_valor row ">
-        <div class="caixa_de_valor col col-md-auto ">
+    <div class="conteine_valor">
+        <div class="caixa_de_valor">
             valor total R$
+            <div class="preço_atual" id="preco_atual">00.00</div>
         </div>
-        <div class="preço_atual col col-md-auto" id="preco_atual">00.00</div>
-        <div class="button_envia_pedidos"><button class="button_cardapio_adiciona" type="submit">Fazer Pedidos</button>
+        <div class="button_envia_pedidos">
+            <button class="btn btn-info" type="submit">Fazer Pedidos</button>
         </div>
     </div>
 
